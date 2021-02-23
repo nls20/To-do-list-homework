4f64078de2237924e21b43639b2211fade02b287
nls20 <nls20@hotmail.co.uk>
2021-02-23 08:57:29 +0000

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index"
+      v-bind:class="todo.priorityHigh ? 'priorityHigh' : 'priorityLow' ">
+        <span>{{todo.name}}</span>
+        <span>{{todo.priority}}</span>
+        <button v-if="todo.priorityHigh" @click="clickTodo(index)">Make Low</button>
+        <button v-if="todo.priorityLow" @click="clickTodo(index)">Make High</button>
+      </li>
+    </ul>
+
+    <form v-on:submit.prevent="saveNewTodo">
+      <label for="new-todo">Add a new To-do Task: </label>
+      <input type="text" id="new-todo" v-model="newTodo">
+      <input type="submit" value="Save New To-do Task">
+    </form>
+    <form v-on:submit.prevent="savePriority">
+      <input type="radio" id="high" value="true" v-model="picked">
+      <label for="high">High</label>
+      <input type="radio" id="low" value="false" v-model="picked">
+      <label for="low">Low</label>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+ data() {
+   return {
+     todos: [
+       {name: "Clean Car", priorityHigh: true},
+       {name: "Do Shopping", priorityHigh: true},
+       {name: "Do Dishes", priorityLow: false},
+     ],
+     newTodo: ""
+   }
+ },
+ methods: {
+   saveNewTodo: function() {
+     this.todos.push({
+       name: this.newTodo,
+       priorityHigh: false
+     });
+     this.newTodo = "";
+   
+ },
+  savePriority: function() {
+    this.this.todos.push(
+      this.priority
+    );
+    this.priority = "";
+  },
+
+ clickTodo: function(index) {
+   this.todos[index].priorityHigh = false;
+ }
+}
 }
 </script>
 
@@ -24,5 +67,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+li.priorityHigh {
+  border: 2px solid red;
+}
+
+li.priorityLow {
+  border: 2px solid green;
 }
 </style>
